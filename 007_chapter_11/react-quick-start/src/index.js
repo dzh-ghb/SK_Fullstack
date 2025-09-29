4d97027be78fd906d92ba9c7bac50e56c553d572
dzh <d.snitchester@gmail.com>
2025-09-29 15:06:46 +0300

    007_chapter_11_React_p3

--- 007_chapter_11/react-quick-start/src/index.js
+++ 007_chapter_11/react-quick-start/src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
+const getRandom = () => Math.round(Math.random() * 10);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const firstName = "d";
 const lastName = "zh";
-const val = `100%`;
+// const val = `100%`;
 const arr = [19, 20, 28];
 const currentYear = new Date().getFullYear();
 
@@ -16,11 +18,18 @@ root.render(
     <h2>NDBT</h2>
     <h3>{firstName} {lastName}</h3>
     <ul>
+      {
+        // arr.map(item => (<li>{item}</li>)) // работа с массивом
+        // Array.from({ length: 10 }, (_, index) => // если val не используем - заменяем на "_"
+        //   <li>{`Элемент ${index + 1}`}</li>)
+        Array.from({ length: 10 }, (_, index) =>
+          <li>{`Элемент ${getRandom() % 2 === 0 ? 'EVEN' : 'ODD'}`}</li>)
+      }
+    </ul>
+    {/* {<ul>
       <li>{val}</li>
       <li>{arr[0]}</li>
-      <li>{arr[1]}</li>
-      <li>{arr[2]}</li>
-    </ul>
+    </ul>} */}
     <p>&copy; {currentYear}</p>
   </div>
 );
