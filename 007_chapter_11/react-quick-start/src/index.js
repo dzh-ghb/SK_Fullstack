2ee87e893097ac20fa731c48be57d3eb0edb7bf7
dzh <d.snitchester@gmail.com>
2025-10-02 22:54:09 +0300

    007_chapter_11_React_p6

--- 007_chapter_11/react-quick-start/src/index.js
+++ 007_chapter_11/react-quick-start/src/index.js
@@ -4,25 +4,25 @@ import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const val1 = 'DZHITS';
-const val2 = 'NDBT';
+const first = "DZHITS";
+const second = "NDBT";
 const currentYear = new Date().getFullYear();
 
 root.render(
-  <div className='container col-10'>
-    <h1 className="m-3" style={{ color: 'green', fontSize: `25px` }}>{val1}</h1>
-    <h2 className="h1-style m-3">{val2}</h2>
+  <div className="container col-8">
+    <h1 className='h1-style m-3'>{second}</h1>
 
     {/* фигурные скобки нужны, так как это JS-вкрапление */}
-    {Array.from({ length: 5 }, (_, index) =>
-      <div className="card m-3" style={{ width: `18rem` }}>
-        <img src={`https://cataas.com/cat?width=${320 + index}`} className="card-img-top" alt="..." />
+    {Array.from({ length: 3 }, (_, index) =>
+      <div className="card m-3" key={index}>
+        <img src={`https://cataas.com/cat?width=${index + 1337}`} className="card-img-top" alt="..." />
         <div className="card-body">
-          <h5 className="card-title">Card title</h5>
+          <h5 className="card-title">{first} {'#' + index}</h5>
           <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
         </div>
-      </div>)}
+      </div>
+    )}
 
-    <p className="m-3">&copy; {currentYear}</p>
+    <p className='m-3'>&copy; {currentYear}</p>
   </div>
 );
