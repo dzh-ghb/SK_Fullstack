2aecdb0aa3a4202fc9e2ac0c5d7815fd64eb7cb0
dzh <d.snitchester@gmail.com>
2025-10-13 15:22:18 +0300

    007_chapter_11_React_p12

--- 007_chapter_11/react-quick-start/src/layout/HomePage/HomePage.jsx
+++ 007_chapter_11/react-quick-start/src/layout/HomePage/HomePage.jsx
@@ -4,7 +4,7 @@ import HeaderItem from './components/HeaderItem';
 import ContentItem from './components/ContentItem';
 import FooterItem from './components/FooterItem';
 
-// url для запроса к FakeAPI
+// url для запроса к сервису с FakeAPI
 const baseUrl = "https://jsonplaceholder.typicode.com/posts";
 
 // конструктор
@@ -24,24 +24,38 @@ const HomePage = () => {
 
     // GET-запрос и вывод результата в консоль
     useEffect(() => {
-        axios.get(baseUrl)
-            .then(res => console.log(res));
+        axios.get(baseUrl) // запрос к сервису
+            // .then(res => console.log(res));
+            // .then(res => console.log(res.data.slice(0, 5)));
+            .then(res => { // обработка полученных данных, в res - ответ от сервиса
+                const temp = [];
+                res.data.slice(0, 5).forEach( // работа со свойством data со всеми объектами
+                    item => { // перебор каждого объекта
+                        temp.push({ // заполнение временного объекта нужной модели данных (структуры)
+                            id: item.id,
+                            cardTitle: item.title.slice(0, 10),
+                            cardText: item.body.slice(0, 50)
+                        });
+                    }
+                );
+                setLabels(temp); // изменение исходного массива данных labels
+            });
     })
 
     return (
         <div className="container col-8">
             <HeaderItem title={headerTitle} />
             <ContentItem
-                setOfLabels={labels}
+                setOfLabels={labels} // передачи данных для отображения
                 imgSrc={imgSrc}
                 imgAlt={imgAlt}
             />
-            <button className="btn btn-primary m-3"
+            {/* <button className="btn btn-primary m-3"
                 onClick={() => {
                     setLabels([...labels,
                     { id: 1, cardTitle: "New Header #1", cardText: "New Description #1" },
                     ])
-                }}>Add element</button>
+                }}>Add element</button> */}
             <FooterItem datePart={datePart} />
         </div>
     )
