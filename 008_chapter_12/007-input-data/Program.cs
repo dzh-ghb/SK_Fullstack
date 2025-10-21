8a292ce07b5211ea49d19ec7c470587c3f89b564
dzh <d.snitchester@gmail.com>
2025-10-21 22:34:51 +0300

    008_chapter_12_C#_p4

--- 008_chapter_12/007-input-data/Program.cs
+++ 008_chapter_12/007-input-data/Program.cs
@@ -1,24 +1,34 @@
-﻿// Пример ввода строки с консоли
-Console.Write("Введите ваше имя: ");
-string name = Console.ReadLine();
-// Вывод приветствия с именем пользователя
-Console.WriteLine($"Привет, {name}!");
-Console.WriteLine("Привет, " + name + "!");
+﻿// // Пример ввода строки с консоли
+// Console.Write("Введите ваше имя: ");
+// string name = Console.ReadLine();
+// // Вывод приветствия с именем пользователя
+// Console.WriteLine($"Привет, {name}!"); // интерполяция строк (значение подставляется во время компиляции)
+// Console.WriteLine("Привет, " + name + "!");
 
-// Пример ввода одного символа с консоли
-Console.Write("Нажмите любую клавишу, чтобы продолжить...");
+// // Пример ввода одного символа с консоли
+// Console.Write("Нажмите любую клавишу, чтобы продолжить...");
+// ConsoleKeyInfo keyInfo = Console.ReadKey();
+// // Вывод информации о нажатой клавише
+// Console.WriteLine($"\nВы нажали клавишу {keyInfo.KeyChar}.");
+
+// // Пример обработки числового ввода
+// Console.Write("Введите ваш возраст: ");
+// string inputAge = Console.ReadLine();
+
+// // Попытка преобразовать строку в число
+// int age = default;
+// bool parseResult = int.TryParse(inputAge, out age);
+// // Вывод возраста, если ввод корректен
+// Console.WriteLine($"Получилось ли преобразовать: {parseResult}");
+
+Console.WriteLine("Input #1");
+string s1 = Console.ReadLine();
+Console.WriteLine($"Input >> {s1}");
+
+Console.WriteLine("Press any key...");
 ConsoleKeyInfo keyInfo = Console.ReadKey();
-// Вывод информации о нажатой клавише
-Console.WriteLine($"\nВы нажали клавишу {keyInfo.KeyChar}.");
+Console.WriteLine($"\npressed {keyInfo.KeyChar} key");
 
-// Пример обработки числового ввода
-Console.Write("Введите ваш возраст: ");
-string inputAge = Console.ReadLine();
-
-// Попытка преобразовать строку в число
-int age = default;
-bool parseResult = int.TryParse(inputAge, out age);
-// Вывод возраста, если ввод корректен
-Console.WriteLine($"Получилось ли преобразовать: {parseResult}");
-Console.WriteLine($"Ваш возраст: {age} лет.");
-
+Console.WriteLine("Input #2");
+bool parseResult = int.TryParse(Console.ReadLine(), out int age);
+Console.WriteLine($"Success? {parseResult}");
--- 008_chapter_12/008-demo-tip-calculator/Program.cs
+++ 008_chapter_12/008-demo-tip-calculator/Program.cs
@@ -1,16 +1,31 @@
-﻿Console.Write("Введите сумму счёта: ");
+﻿Console.Write("Введите сумму счета: ");
 double billAmount = double.Parse(Console.ReadLine());
-
 Console.Write("Введите процент чаевых: ");
 double tipPercentage = double.Parse(Console.ReadLine());
-
-Console.Write("Введите количество человек: ");
+Console.Write("Введите количество людей: ");
 int numberOfPeople = int.Parse(Console.ReadLine());
 
-double tipAmount = billAmount * tipPercentage / 100;
+double tipAmount = billAmount * tipPercentage * 0.01;
 double totalAmount = billAmount + tipAmount;
 double perPersonAmount = totalAmount / numberOfPeople;
 
-Console.WriteLine($"Общий счёт: {totalAmount}");
+Console.WriteLine($"Общий счет: {Math.Round(totalAmount, 2)}");
 Console.WriteLine($"Процент чаевых: {tipPercentage}%");
-Console.WriteLine($"С каждого: {perPersonAmount}");
+Console.WriteLine($"Сумма на каждого: {Math.Round(perPersonAmount, 2)}");
+
+// Console.Write("Введите сумму счёта: ");
+// double billAmount = double.Parse(Console.ReadLine());
+
+// Console.Write("Введите процент чаевых: ");
+// double tipPercentage = double.Parse(Console.ReadLine());
+
+// Console.Write("Введите количество человек: ");
+// int numberOfPeople = int.Parse(Console.ReadLine());
+
+// double tipAmount = billAmount * tipPercentage / 100;
+// double totalAmount = billAmount + tipAmount;
+// double perPersonAmount = totalAmount / numberOfPeople;
+
+// Console.WriteLine($"Общий счёт: {totalAmount}");
+// Console.WriteLine($"Процент чаевых: {tipPercentage}%");
+// Console.WriteLine($"С каждого: {perPersonAmount}");
