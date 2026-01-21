beb3d760b58d3c86be90099d6a39caf98fd2f973
dzh <d.snitchester@gmail.com>
2026-01-21 23:05:16 +0300

    013_chapter_18_Backend_n_DB_p7

---
+++ 013_chapter_18/002-test/Program.cs
@@ -0,0 +1,25 @@
+﻿// int i = 251;
+// double res = Math.Sqrt(i);
+// System.Console.WriteLine(res);
+
+// System.Console.WriteLine(i.Sqrt());
+
+// i.Sqrt().Print();
+// 251.Sqrt(0).Print();
+251.Sqrt().Print();
+251.12.Print();
+"251".Print();
+
+// создание методов расширений (доп. функционал для типов данных)
+public static class Extension
+{
+    public static double Sqrt(this int x, int count = 2) // пример метода расширений
+    {
+        return Math.Round(Math.Sqrt(x), count);
+    }
+
+    public static void Print<T>(this T x) // обобщенный метод расширения
+    {
+        Console.WriteLine(x);
+    }
+}
