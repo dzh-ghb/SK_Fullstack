40ffe022d68d25125e51c07e1c03146892580ff9
dzh <d.snitchester@gmail.com>
2026-01-25 00:22:38 +0300

    013_chapter_18_Backend_n_DB_p9

---
+++ 013_chapter_18/003-mock/Program.cs
@@ -0,0 +1,24 @@
+﻿using Bogus;
+
+var faker = new Faker<Contact>("ru")
+    .RuleFor(c => c.FullName, f => f.Name.FullName()) // сопоставление нашей модели и фейк-данных
+    .RuleFor(c => c.Email, f => f.Internet.Email());
+
+var data = faker.Generate(10); // создание коллекции данных
+
+foreach (var item in data)
+{
+    Console.WriteLine(item);
+}
+
+// нужная модель данных
+class Contact
+{
+    public string FullName { get; set; }
+    public string Email { get; set; }
+
+    public override string ToString()
+    {
+        return $"{this.FullName}, {this.Email}";
+    }
+}
